405311dc0f46b24fc199af38e35632613600eee0
Ali <superman78@usa.com>
2020-01-29 22:58:55 +0000

    first commit:

---
+++ deploy_eks.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export EKS_CLUSTER=apigw-demo
+export NODE_COUNT=3
+export NODE_TYPE=t3.small
+export AWS_REGION=us-east-2
+
+if [ "$(aws eks describe-cluster --name ${EKS_CLUSTER})" != "" ]; then
+	echo "${EKS_CLUSTER} EKS cluster already created!"
+        exit 0
+fi
+
+# deploy eks cluster with 3 managed nodes
+eksctl create cluster --name=${EKS_CLUSTER} \
+		--nodes=${NODE_COUNT} \
+		--managed \
+		--node-type ${NODE_TYPE} \
+		--alb-ingress-access \
+		--region=${AWS_REGION}
