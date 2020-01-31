06a414109ba3f7daab645e2fc69844d278a60cfc
adam <adam@meerutech.com>
2020-01-31 16:31:49 +0000

    fluxctl

--- 5_deploy_ssh_key.sh
+++ 5_deploy_ssh_key.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+wget https://github.com/fluxcd/flux/releases/download/1.14.2/fluxctl_linux_amd64 -O /usr/local/bin/fluxctl
+chmod +x /usr/local/bin/fluxctl
+
 GITHUB_API_TOKEN=$(aws secretsmanager get-secret-value --secret-id github-api-token --region us-east-2 | jq -r .SecretString)
 GITHUB_USER=alimeerutech
 KEY=$(fluxctl --k8s-fwd-ns flux identity)
